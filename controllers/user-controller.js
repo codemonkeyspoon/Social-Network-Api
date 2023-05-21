d4bff4668c42bf7f61794082499a83047bcf0fe4
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-05-21 15:33:00 -0400

    refactoring user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,60 +1,65 @@
 const {User, Thought} = require('../models');
 
 const userController = {
-    //Get all users
-    getAllUsers(req, res) {
-        User.find({})
-        .populate([
+    // Get all users
+    async getAllUsers(req, res) {
+      try {
+        const dbUserData = await User.find({})
+          .populate([
             {
-                path: "thoughts",
-                select: "-__v"
+              path: "thoughts",
+              select: "-__v",
             },
             {
-                path: "friends",
-                select: "-__v"
-            }
-        ])
-        .select("-__v")
-        .sort({_id: -1})
-        .then((dbUserData) => res.json(dbUserData))
-        .catch((err) => {
-            console.log(err);
-            res.status(400).json(err)
-        });
+              path: "friends",
+              select: "-__v",
+            },
+          ])
+          .select("-__v")
+          .sort({ _id: -1 });
+  
+        res.json(dbUserData);
+      } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+      }
     },
     // Get single user
-    getUserById({params}, res) {
-        User.findOne({_id: params.id})
-        .populate([
+    async getUserById({ params }, res) {
+      try {
+        const dbUserData = await User.findOne({ _id: params.id })
+          .populate([
             {
-                path: "thoughts",
-                select: "-__v"
+              path: "thoughts",
+              select: "-__v",
             },
             {
-                path: "friends",
-                select: "-__v"
-            }
-        ])
-        .select("-__v")
-        .then((dbUserData) => {
-            if (!dbUserData) {
-                res.status(404).json({ message: "No User found with this id "});
-                return;
-            }
-            res.json(dbUserData)
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(404).json(err);
-        });
+              path: "friends",
+              select: "-__v",
+            },
+          ])
+          .select("-__v");
+  
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id" });
+          return;
+        }
+  
+        res.json(dbUserData);
+      } catch (err) {
+        console.log(err);
+        res.status(404).json(err);
+      }
     },
-    //Create User
-    createUser({ body }, res) {
-        User.create(body)
-        .then((dbUserData) => res.json(dbUserData))
-        .catch((err) => res.status(400).json(err));
-    }
-}
-
-
-module.exports = userController;
+    // Create User
+    async createUser({ body }, res) {
+      try {
+        const dbUserData = await User.create(body);
+        res.json(dbUserData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
+    },
+  };
+  
+  module.exports = userController;
