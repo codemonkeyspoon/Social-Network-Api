ca0cfa389e51dc4b876c35d057fddd311a659b5b
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-05-21 15:28:12 -0400

    added get route for single user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -22,6 +22,32 @@ const userController = {
             res.status(400).json(err)
         });
     },
+    // Get single user
+    getUserById({params}, res) {
+        User.findOne({_id: params.id})
+        .populate([
+            {
+                path: "thoughts",
+                select: "-__v"
+            },
+            {
+                path: "friends",
+                select: "-__v"
+            }
+        ])
+        .select("-__v")
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No User found with this id "});
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(404).json(err);
+        });
+    },
     //Create User
     createUser({ body }, res) {
         User.create(body)
